90034acc9533728bbad32958e662f587af50a425
Nikita Peplov <npeplov@yandex.ru>
2021-03-05 08:35:09 +0300

    origin

---
+++ env/src/js/app.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+// TODO: write code here
+// document.getElementsByTagName('img')[0].setAttribute('alt', 'img/netology.svg');
+
+[1, 2, 3].forEach((elem) => {
+  console.log(elem);
+});
---
+++ env/webpack.prod.js
@@ -0,0 +1,15 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({}),
+      new TerserPlugin({}),
+    ],
+  },
+});
