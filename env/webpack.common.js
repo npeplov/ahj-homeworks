ec03797f6e45b35ff63351443747ca5c0c9474b6
Nikita Peplov <npeplov@yandex.ru>
2021-03-05 08:56:45 +0300

    publicPath: / addded

--- env/webpack.common.js
+++ env/webpack.common.js
@@ -6,6 +6,7 @@ module.exports = {
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     assetModuleFilename: 'images/[name][contenthash][ext]'
   },
   module: {
